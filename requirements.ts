5d7fd4c3f675652c30f0f1adb1587de5f331e862
Guna <111410349+Guna1996@users.noreply.github.com>
2025-04-05 20:14:28 +0530

    Update requirements.ts

--- requirements.ts
+++ requirements.ts
@@ -1,6 +1,6 @@
-google-generativeai
-requests
-SpeechRecognition
-python-telegram-bot
-gTTS
-pydub
+google-generativeai==0.8.4
+requests==2.32.3
+SpeechRecognition==3.14.2
+python-telegram-bot==22.0
+gTTS==2.5.4
+pydub==0.25.1

db4813342b710e31905530ff5b72089cf1ff4be7
Guna <111410349+Guna1996@users.noreply.github.com>
2025-04-11 16:47:24 +0530

    Token fixed

--- TeleGptBot.py
+++ TeleGptBot.py
@@ -15,8 +15,8 @@ import logging
 # Bot configuration
 for key, value in os.environ.items():
     print(f"{key}: {value[:3]}..." if value else f"{key}: None")
-TOKEN: Final = os.environ.get('BOT_TOKEN')
-# TOKEN: Final = '1332501115:AAHOVj2bTdGydfU5ye57ktebymzufLRaSGY'
+# TOKEN: Final = os.environ.get('BOT_TOKEN')
+TOKEN: Final = '1707467959:AAHZXmclpNgvdx8ZtWqA1v9njlfja2safjI'
 BOT_USERNAME: Final = '@Lilly007_bot'
 
 # Google Gemini configuration
